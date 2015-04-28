9d40ca7c2d41084fe2c506bd9f4bcc84e2abea3b
joren <jhammudoglu@gmail.com>
2015-04-28 16:42:29 +0200

    Created basic application layout.

--- src/nl/tudelft/lifetiles/Main.java
+++ src/nl/tudelft/lifetiles/Main.java
@@ -1,25 +1,29 @@
 package nl.tudelft.lifetiles;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			Parent root = FXMLLoader.load(getClass().getResource(
+					"Main.fxml"));
+			Scene scene = new Scene(root, 1280, 720);
+			scene.getStylesheets().add(
+					getClass().getResource("application.css").toExternalForm());
+			
+			stage.setTitle("LifeTiles");
+	        stage.setScene(scene);
+	        stage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
