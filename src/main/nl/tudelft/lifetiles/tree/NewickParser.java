1d6750060b43ef5be79a318b87726e7d4c3c07bd
Albert Smit <albertsmit93@gmail.com>
2015-04-30 23:04:24 +0200

    added comments to the parser

--- src/main/nl/tudelft/lifetiles/tree/NewickParser.java
+++ src/main/nl/tudelft/lifetiles/tree/NewickParser.java
@@ -6,6 +6,11 @@ public class NewickParser {
 	private PhylogeneticTreeItem root;
 	private String treeFile;
 		
+	/**
+	 * Creates a new NewickParser that parses the String
+	 * containing a single tree and creates a tree of PhylogeneticTreeItems 
+	 * @param tree a String specifying a single tree in newick format.
+	 */
 	public NewickParser(String tree){
 		treeFile = tree;
 		root = new PhylogeneticTreeItem();
@@ -14,26 +19,33 @@ public class NewickParser {
 		
 	}
 	
+	/**
+	 * parses the String the parser has been given.
+	 * the String is first tokenized, after which the method iterates 
+	 * over all tokens
+	 */
 	private void parse(){
+	    // split the string ino tokens, and keep the delimiters in the list so we have
+	    // information about the next token.
 		StringTokenizer st = new StringTokenizer(treeFile,"(:,);",true);
+		
 		PhylogeneticTreeItem currentNode = root;
+		// loop over all tokens
 		while(st.hasMoreTokens()){
 			String currentToken = st.nextToken();
-			
-			
-			// list of children
+			// this starts a list of child nodes, add a child node and make it our current node
 			if(currentToken.equals("(")){
 				PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
 				newChild.setParent(currentNode);
 				currentNode = newChild;
 			}
-			// next token is a distance
+			// next token is a distance, add it to the current node
 			else if(currentToken.equals(":")){
 				String distanceToken = st.nextToken();
 				double distance = Double.parseDouble(distanceToken);
 				currentNode.setDistance(distance);
 			}
-			// next token is another child
+			// next token is another child, add a new child to the list and make it the current node
 			else if(currentToken.equals(",")){
 				currentNode = currentNode.getParent();
 				PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
@@ -42,15 +54,16 @@ public class NewickParser {
 					
 				
 				
-			}// list of children ends
+			}// list of children ends, go up 1 level so we can add a name and distance to this node
+			// if they are specified
 			else if(currentToken.equals(")")){
 				currentNode = currentNode.getParent();
 			}
-			// tree is done
+			// tree is done, nothing special needs to be done
 			else if(currentToken.equals(";")){
 				break;
 			}
-			// final case current token is a name
+			// final case, the current token is text, so the current token is a name
 			else{
 				currentNode.setName(currentToken);
 			}
@@ -58,7 +71,10 @@ public class NewickParser {
 		
 		
 	}
-	
+	/**
+	 * Returns the parsed tree.
+	 * @return the root node of the tree.
+	 */
 	public PhylogeneticTreeItem getRoot(){
 		return root;
 	}
