8e395fe567f1cc4c4189eef423664a80cef7c0fc
Albert Smit <albertsmit93@gmail.com>
2015-05-01 16:14:28 +0200

    fixed checkstyle errors

--- src/main/java/nl/tudelft/lifetiles/tree/NewickParser.java
+++ src/main/java/nl/tudelft/lifetiles/tree/NewickParser.java
@@ -1,82 +1,98 @@
 package nl.tudelft.lifetiles.tree;
 
 import java.util.StringTokenizer;
+/**
+ * A simple parser that will parse a single tree in newick format
+ * to a simple tree.
+ * @author Albert Smit
+ *
+ */
 
 public class NewickParser {
-	private PhylogeneticTreeItem root;
-	private String treeFile;
-		
-	/**
-	 * Creates a new NewickParser that parses the String
-	 * containing a single tree and creates a tree of PhylogeneticTreeItems 
-	 * @param tree a String specifying a single tree in newick format.
-	 */
-	public NewickParser(String tree){
-		treeFile = tree;
-		root = new PhylogeneticTreeItem();
-		
-		parse();
-		
-	}
-	
-	/**
-	 * parses the String the parser has been given.
-	 * the String is first tokenized, after which the method iterates 
-	 * over all tokens
-	 */
-	private void parse(){
-	    // split the string ino tokens, and keep the delimiters in the list so we have
-	    // information about the next token.
-		StringTokenizer st = new StringTokenizer(treeFile,"(:,);",true);
-		
-		PhylogeneticTreeItem currentNode = root;
-		// loop over all tokens
-		while(st.hasMoreTokens()){
-			String currentToken = st.nextToken();
-			// this starts a list of child nodes, add a child node and make it our current node
-			if(currentToken.equals("(")){
-				PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
-				newChild.setParent(currentNode);
-				currentNode = newChild;
-			}
-			// next token is a distance, add it to the current node
-			else if(currentToken.equals(":")){
-				String distanceToken = st.nextToken();
-				double distance = Double.parseDouble(distanceToken);
-				currentNode.setDistance(distance);
-			}
-			// next token is another child, add a new child to the list and make it the current node
-			else if(currentToken.equals(",")){
-				currentNode = currentNode.getParent();
-				PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
-				newChild.setParent(currentNode);
-				currentNode = newChild;
-					
-				
-				
-			}// list of children ends, go up 1 level so we can add a name and distance to this node
-			// if they are specified
-			else if(currentToken.equals(")")){
-				currentNode = currentNode.getParent();
-			}
-			// tree is done, nothing special needs to be done
-			else if(currentToken.equals(";")){
-				break;
-			}
-			// final case, the current token is text, so the current token is a name
-			else{
-				currentNode.setName(currentToken);
-			}
-		}
-		
-		
-	}
-	/**
-	 * Returns the parsed tree.
-	 * @return the root node of the tree.
-	 */
-	public PhylogeneticTreeItem getRoot(){
-		return root;
-	}
+    /**
+     * The root node of the created tree.
+     */
+    private PhylogeneticTreeItem root;
+
+    /**
+     * The String specifying a single tree in newick format.
+     */
+    private String treeFile;
+
+    /**
+     * Creates a new NewickParser that parses the String containing a single.
+     * tree and creates a tree of PhylogeneticTreeItems
+     *
+     * @param tree
+     *            a String specifying a single tree in newick format.
+     */
+    public NewickParser(final String tree) {
+        treeFile = tree;
+        root = new PhylogeneticTreeItem();
+
+        parse();
+
+    }
+
+    /**
+     * parses the String the parser has been given.
+     * the String is first tokenized,
+     * after which the method iterates over all tokens
+     */
+    private void parse() {
+        // split the string ino tokens, and keep the delimiters in the list so
+        // we have
+        // information about the next token.
+        StringTokenizer st = new StringTokenizer(treeFile, "(:,);", true);
+
+        PhylogeneticTreeItem currentNode = root;
+        // loop over all tokens
+        while (st.hasMoreTokens()) {
+            String currentToken = st.nextToken();
+            // this starts a list of child nodes, add a child node and make it
+            // our current node
+            if (currentToken.equals("(")) {
+                PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
+                newChild.setParent(currentNode);
+                currentNode = newChild;
+            } else if (currentToken.equals(":")) {
+                // next token is a distance, add it to the current node
+                String distanceToken = st.nextToken();
+                double distance = Double.parseDouble(distanceToken);
+                currentNode.setDistance(distance);
+            } else if (currentToken.equals(",")) {
+                // next token is another child, add a new child to the list and
+                // make it the current node
+                currentNode = currentNode.getParent();
+                PhylogeneticTreeItem newChild = new PhylogeneticTreeItem();
+                newChild.setParent(currentNode);
+                currentNode = newChild;
+
+            } else if (currentToken.equals(")")) {
+                // list of children ends, go up 1 level so we can add a name and
+                // distance to this node
+                // if they are specified
+                currentNode = currentNode.getParent();
+            } else if (currentToken.equals(";")) {
+                // tree is done, nothing special needs to be done
+                break;
+            } else {
+                // final case, the current token is text,
+                // so the current token is a name.
+
+                currentNode.setName(currentToken);
+            }
+        }
+
+    }
+
+    /**
+     * Returns the parsed tree.
+     *
+     * @return the root node of the tree.
+     */
+    public final PhylogeneticTreeItem getRoot() {
+        return root;
+    }
 
 }
--- src/main/java/nl/tudelft/lifetiles/tree/PhylogeneticTreeItem.java
+++ src/main/java/nl/tudelft/lifetiles/tree/PhylogeneticTreeItem.java
@@ -4,126 +4,158 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /***
- * 
- * @author albert
+ * a tree to store the relation between samples.
+ *
+ * @author Albert Smit
  *
  */
 public class PhylogeneticTreeItem {
-	private static AtomicInteger nextID = new AtomicInteger();
-    
-	private PhylogeneticTreeItem parent;
-	private ArrayList<PhylogeneticTreeItem> children;
-	
-	private String name;
-	private double distance;
-	private int id;
-	
-	/**
-	 * Creates a new PhylogeneticTreeItem.
-	 * Will initialize the ArrayList storing the children and
-	 * assign a new and unique id to the node. 
-	 */
-	public PhylogeneticTreeItem(){
-		children = new ArrayList<PhylogeneticTreeItem>();
-		this.id = nextID.incrementAndGet();
-	}
-	/**
-	 * Returns a String representation of the PhylogeneticTreeItem.
-	 * The String will have the following format:
-	 * <Node: id, name: name, Distance: distance, parent: parentID>
-	 * or when the PhylogeneticTreeItem has no parent:
-	 * <Node: id, Name: name, Distance: distance, ROOT>
-	 */
-	public String toString(){
-		String result ="<Node: " + id + ", Name: " + name + ", Distance: " + distance ;
-		if(parent != null){
-			result += ", parent: "+ parent.getId();
-		}
-		else{
-			result += ", ROOT ";
-		}
-		return  result + ">";
-	}
-	
-	/**
-	 * Adds a child to the PhylogeneticTreeItem.
-	 * This method will add the PhylogeneticTreeItem child to the arraylist storing the children
-	 * of this node
-	 * @param child the PhylogeneticTreeItem that needs to be added to the tree
-	 */
-	public void addChild(PhylogeneticTreeItem child){
-		children.add(child);
-	}
-	
-	/** 
-	 * Returns the ArrayList of children.
-	 * @return the ArrayList containing all children of this node
-	 */
-	public ArrayList<PhylogeneticTreeItem> getChildren(){
-		return children;
-	}
-	
-	/**
-	 * Sets the parent to be the node passed to the method
-	 * This method will set the parent and also add itself to the list of children
-	 * in the parent node.
-	 * @param parent the node that will be this nodes parent
-	 */
-	public void setParent(PhylogeneticTreeItem parent){
-		this.parent = parent;
-		this.parent.addChild(this);
-	}
-	/**
-	 * Returns this nodes parent node.
-	 * @return the PhylogeneticTreeItem that is this nodes parent
-	 */
-	public PhylogeneticTreeItem getParent(){
-		return parent;
-	}
-	
-	/**
-	 * Returns the name stored in this node.
-	 * name is an optional property, so this method can return null.
-	 * @return the name of this node
-	 */
-	public String getName() {
-		return name;
-	}
-	
-	/**
-	 * Set this nodes name to the passed String.
-	 * @param name the name of this node
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-     * Returns the distance stored in this node.
-     * distance is an optional property, so the distance can often be 0.0.
+    /**
+     * Used to give each node a unique id.
+     */
+    private static AtomicInteger nextID = new AtomicInteger();
+
+    /**
+     * The parent node, null when this node is the root node.
+     */
+    private PhylogeneticTreeItem parent;
+    /**
+     * The list of children of this node.
+     */
+    private ArrayList<PhylogeneticTreeItem> children;
+
+    /**
+     * The name of the sample. This is an optional field.
+     */
+    private String name;
+    /**
+     * The distance between samples. This is an optinal field.
+     */
+    private double distance;
+    /**
+     * A unique id for each node.
+     */
+    private int id;
+
+    /**
+     * Creates a new PhylogeneticTreeItem. Will initialize the ArrayList storing
+     * the children and assign a new and unique id to the node.
+     */
+    public PhylogeneticTreeItem() {
+        children = new ArrayList<PhylogeneticTreeItem>();
+        this.id = nextID.incrementAndGet();
+    }
+
+    /**
+     * Returns a String representation of the PhylogeneticTreeItem. The String
+     * will have the following format: <Node: id, name: name, Distance:
+     * distance, parent: parentID> or when the PhylogeneticTreeItem has no
+     * parent: <Node: id, Name: name, Distance: distance, ROOT>
+     *
+     * @return the String version of the object.
+     */
+    public final String toString() {
+        String result = "<Node: " + id + ", Name: " + name + ", Distance: "
+                + distance;
+        if (parent != null) {
+            result += ", parent: " + parent.getId();
+        } else {
+            result += ", ROOT ";
+        }
+        return result + ">";
+    }
+
+    /**
+     * Adds a child to the PhylogeneticTreeItem. This method will add the
+     * PhylogeneticTreeItem child to the ArrayList storing the children of this
+     * node
+     *
+     * @param child
+     *            the PhylogeneticTreeItem that needs to be added to the tree
+     */
+    public final void addChild(final PhylogeneticTreeItem child) {
+        children.add(child);
+    }
+
+    /**
+     * Returns the ArrayList of children.
+     *
+     * @return the ArrayList containing all children of this node
+     */
+    public final ArrayList<PhylogeneticTreeItem> getChildren() {
+        return children;
+    }
+
+    /**
+     * Sets the parent to be the node passed to the method This method will set
+     * the parent and also add itself to the list of children in the parent
+     * node.
+     *
+     * @param parentNode
+     *            the node that will be this nodes parent
+     */
+    public final void setParent(final PhylogeneticTreeItem parentNode) {
+        this.parent = parentNode;
+        this.parent.addChild(this);
+    }
+
+    /**
+     * Returns this nodes parent node.
+     *
+     * @return the PhylogeneticTreeItem that is this nodes parent
+     */
+    public final PhylogeneticTreeItem getParent() {
+        return parent;
+    }
+
+    /**
+     * Returns the name stored in this node. name is an optional property, so
+     * this method can return null.
+     *
+     * @return the name of this node
+     */
+    public final String getName() {
+        return name;
+    }
+
+    /**
+     * Set this nodes name to the passed String.
+     *
+     * @param n
+     *            the name of this node
+     */
+    public final void setName(final String n) {
+        this.name = n;
+    }
+
+    /**
+     * Returns the distance stored in this node. distance is an optional
+     * property, so the distance can often be 0.0.
+     *
      * @return the distance of this node
      */
-	public double getDistance() {
-		return distance;
-	}
-	
-	/**
-	 * Sets this nodes distance to the passed double.
-	 * @param distance the distance between the nodes
-	 */
-	public void setDistance(double distance) {
-		this.distance = distance;
-	}
+    public final double getDistance() {
+        return distance;
+    }
 
-	/**
-	 * Returns the Id of this node.
-	 * because name and distance are optional this provides an easy way of identifying nodes
-	 * @return the unique id of this PhylogeneticTreeItem
-	 */
-	public int getId() {
-		return id;
-	}
+    /**
+     * Sets this nodes distance to the passed double.
+     *
+     * @param d
+     *            the distance between the nodes
+     */
+    public final void setDistance(final double d) {
+        this.distance = d;
+    }
 
-
+    /**
+     * Returns the Id of this node. because name and distance are optional this
+     * provides an easy way of identifying nodes
+     *
+     * @return the unique id of this PhylogeneticTreeItem
+     */
+    public final int getId() {
+        return id;
+    }
 
 }
