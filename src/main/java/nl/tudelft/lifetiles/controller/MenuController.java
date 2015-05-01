0e82af913ecf4b0e851891450d861a1ec412bb08
joren <jhammudoglu@gmail.com>
2015-05-01 13:12:00 +0200

    Added functionality to the window control buttons. Made the window draggable from the menu bar.

--- src/main/java/nl/tudelft/lifetiles/controller/MenuController.java
+++ src/main/java/nl/tudelft/lifetiles/controller/MenuController.java
@@ -4,12 +4,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.MenuBar;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 /**
  * The controller of the menu bar.
@@ -23,6 +27,9 @@ public class MenuController implements Initializable {
 	 */
 	@FXML
 	private MenuBar menuBar;
+
+	protected double initialX;
+	protected double initialY;
 
 	/**
 	 * Handle action related to "About" menu item.
@@ -53,6 +60,38 @@ public class MenuController implements Initializable {
 	}
 
 	/**
+	 * Make a node draggable so that when draggin that node, the window moves.
+	 * 
+	 * @param node
+	 *            the node
+	 */
+	private void addDraggableNode(final Node node) {
+
+		node.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(final MouseEvent me) {
+				if (me.getButton() != MouseButton.MIDDLE) {
+					initialX = me.getSceneX();
+					initialY = me.getSceneY();
+				}
+			}
+		});
+
+		node.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(final MouseEvent me) {
+				if (me.getButton() != MouseButton.MIDDLE) {
+					node.getScene().getWindow()
+							.setX(me.getScreenX() - initialX);
+					node.getScene().getWindow()
+							.setY(me.getScreenY() - initialY);
+				}
+			}
+		});
+	}
+
+	/**
 	 * Perform functionality associated with "About" menu selection or CTRL-A.
 	 */
 	private void provideAboutFunctionality() {
@@ -61,6 +100,6 @@ public class MenuController implements Initializable {
 
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
-		// menuBar.setFocusTraversable(true);
+		this.addDraggableNode(menuBar);
 	}
 }
