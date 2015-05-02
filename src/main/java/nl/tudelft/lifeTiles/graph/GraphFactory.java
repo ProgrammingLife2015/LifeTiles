c2604722bebb19bc3dc3760b77c6183dde885a57
Rutger van den Berg <rutgervandenberg@hotmail.com>
2015-05-02 13:43:29 +0200

    Implement Graph.

--- src/main/java/nl/tudelft/lifeTiles/graph/GraphFactory.java
+++ src/main/java/nl/tudelft/lifeTiles/graph/GraphFactory.java
@@ -10,11 +10,4 @@ public interface GraphFactory<V> {
      * @return a new empty graph.
      */
     Graph<V> getGraph();
-
-    /**
-     * @return A new edge from <code>v1</code> to <code>v2</code>
-     * @param v1 The source vertex.
-     * @param v2 The destination vertex.
-     */
-    DirectedEdge<V> getEdge(final V v1, final V v2);
 }
