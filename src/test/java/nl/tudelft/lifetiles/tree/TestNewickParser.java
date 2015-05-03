7888cdc7517f9f5b2342cfab0cc02c30f63b6400
Albert Smit <albertsmit93@gmail.com>
2015-05-03 21:57:13 +0200

    added more test cases to parser test

--- src/test/java/nl/tudelft/lifetiles/tree/TestNewickParser.java
+++ src/test/java/nl/tudelft/lifetiles/tree/TestNewickParser.java
@@ -42,12 +42,11 @@ public class TestNewickParser {
 
         // compare the trees
         assertEquals("both trees do not match",rootExpected,rootActual);
-
         
     }
 
     /**
-     * test the parser without any data in the tree.
+     * test the parser with named leaf nodes.
      */
     @Test
     public void testNewickParserNamedLeafsNoDistance() {
@@ -79,15 +78,127 @@ public class TestNewickParser {
 
         // compare the trees
         assertEquals("both trees do not match",rootExpected,rootActual);
-
         
     }
+    
     /**
-     * Test method for {@link nl.tudelft.lifetiles.tree.NewickParser#getRoot()}.
+     * test the parser with named nodes.
      */
     @Test
-    public void testGetRoot() {
-        fail("Not yet implemented");
+    public void testNewickParserNamednodesNoDistance() {
+        // create the actual tree
+        String tree = "(A,B,(C,D)E)F;";
+        NewickParser np = new NewickParser(tree);
+        assertTrue("Parser constructor failed, np is NULL", np != null);
+        PhylogeneticTreeItem rootActual = np.getRoot();
+
+        // create the expected Tree
+        // root node
+        PhylogeneticTreeItem rootExpected = new PhylogeneticTreeItem();
+        rootExpected.setName("F");
+        // add 3 child nodes
+        PhylogeneticTreeItem current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setName("A");
+        current = new PhylogeneticTreeItem();
+        current.setName("B");
+        current.setParent(rootExpected);
+        current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setName("E");
+        // add 2 child nodes to the third node
+        PhylogeneticTreeItem current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setName("C");
+        current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setName("D");
+
+        // compare the trees
+        assertEquals("both trees do not match",rootExpected,rootActual);
+       
     }
+    
+    /**
+     * test the parser with unnamed nodes and distances.
+     */
+    @Test
+    public void testNewickParserUnnamednodesAndDistance() {
+        // create the actual tree
+        String tree = "(:0.1,:0.2,(:0.3,:0.4):0.5);";
+        NewickParser np = new NewickParser(tree);
+        assertTrue("Parser constructor failed, np is NULL", np != null);
+        PhylogeneticTreeItem rootActual = np.getRoot();
+
+        // create the expected Tree
+        // root node
+        PhylogeneticTreeItem rootExpected = new PhylogeneticTreeItem();
+        // add 3 child nodes
+        PhylogeneticTreeItem current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setDistance(0.1);
+        current = new PhylogeneticTreeItem();
+        current.setDistance(0.2);
+        current.setParent(rootExpected);
+        current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setDistance(0.5);
+        // add 2 child nodes to the third node
+        PhylogeneticTreeItem current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setDistance(0.4);
+        current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setDistance(0.3);
+
+        // compare the trees
+        assertEquals("both trees do not match",rootExpected,rootActual);
+       
+    }
+    
+    /**
+     * test the parser with named nodes and distance.
+     */
+    @Test
+    public void testNewickParserNamednodesAndDistance() {
+        // create the actual tree
+        String tree = "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;";
+        NewickParser np = new NewickParser(tree);
+        assertTrue("Parser constructor failed, np is NULL", np != null);
+        PhylogeneticTreeItem rootActual = np.getRoot();
+
+        // create the expected Tree
+        // root node
+        PhylogeneticTreeItem rootExpected = new PhylogeneticTreeItem();
+        rootExpected.setName("F");
+        // add 3 child nodes
+        PhylogeneticTreeItem current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setName("A");
+        current.setDistance(0.1);
+        current = new PhylogeneticTreeItem();
+        current.setName("B");
+        current.setDistance(0.2);
+        current.setParent(rootExpected);
+        current = new PhylogeneticTreeItem();
+        current.setParent(rootExpected);
+        current.setName("E");
+        current.setDistance(0.5);
+        // add 2 child nodes to the third node
+        PhylogeneticTreeItem current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setName("C");
+        current2.setDistance(0.3);
+        current2 = new PhylogeneticTreeItem();
+        current2.setParent(current);
+        current2.setName("D");
+        current2.setDistance(0.4);
+        
+
+        // compare the trees
+        assertEquals("both trees do not match",rootExpected,rootActual);
+       
+    }
+
 
 }
