0869db5e3a249fc5e9c45c15701a554a33502d22
joswinter <info@joswinter.nl>
2015-05-04 22:51:26 +0200

    Add GraphTraverser and AlignmentTraverser stubs

---
+++ src/main/java/nl/tudelft/lifetiles/graph/traverser/AlignmentTraverser.java
@@ -0,0 +1,34 @@
+package nl.tudelft.lifetiles.graph.traverser;
+
+import nl.tudelft.lifetiles.graph.Graph;
+import nl.tudelft.lifetiles.graph.SequenceSegment;
+
+/**
+ * Aligns sequences in a graph of sequences, by filling empty space with empty vertices.
+ * @author Jos
+ */
+public class AlignmentTraverser implements GraphTraverser<SequenceSegment> {
+
+	/**
+	 * Traverse a graph and return the aligned graph.
+	 * @param graph
+	 *			The graph that is being aligned.
+	 * @return aligned graph.
+	 */
+	@Override
+	public Graph<SequenceSegment> traverseGraph(Graph<SequenceSegment> graph) {
+		return graph;
+	}
+
+	/**
+	 * Traverse a vertex in the graph and return the aligned vertex.
+	 * @param graph
+	 * @param vertex
+	 *			The vertex that is being traversed.
+	 * @return aligned vertex.
+	 */
+	@Override
+	public SequenceSegment traverseVertex(Graph<SequenceSegment> graph, SequenceSegment vertex) {
+		return vertex;
+	}
+}
