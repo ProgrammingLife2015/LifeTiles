beebd086d225cb98d58d38be281c3e936c71f089
alangerak <a.c.langerak@student.tudelft.nl>
2015-05-08 22:35:02 +0200

    Splitted EdgeLine and removed unnecessary code

--- src/main/java/nl/tudelft/lifetiles/tilegraph/EdgeLine.java
+++ src/main/java/nl/tudelft/lifetiles/tilegraph/EdgeLine.java
@@ -35,70 +35,119 @@ public class EdgeLine extends Group {
      */
     public EdgeLine(final Vertex from, final Vertex to) {
         this.line = new Line();
-
         Bounds boundFrom = from.getBoundsInParent();
         Bounds boundTo = to.getBoundsInParent();
 
+        if (!outofboundsY(boundFrom, boundTo)) {
+            drawStraightLine(from, to, boundFrom, boundTo);
+        } else {
+            drawCrossLine(from, to, boundFrom, boundTo);
+        }
+
+    }
+
+    /**
+     * Draw a straight between two vertices.
+     *
+     * @param from
+     *            - Start Vertex
+     * @param to
+     *            - Destination Vertex
+     * @param boundFrom
+     *            - The Bounds of the start Vertex
+     * @param boundTo
+     *            - The Bounds of the end Vertex
+     */
+    private void drawStraightLine(final Vertex from, final Vertex to,
+            final Bounds boundFrom, final Bounds boundTo) {
         Circle head = new Circle();
         head.setRadius(HEAD_RADIUS);
 
-        if (!outofboundsY(boundFrom, boundTo)) {
+        double startX = boundFrom.getMinX() + (boundFrom.getWidth() / 2);
 
-            line.setStartX(boundFrom.getMinX() + (boundFrom.getWidth() / 2));
+        // The line be drawn from the middle of the vertex to be drawn to
+        if (boundFrom.getMinY() <= boundTo.getMinY()
+                && boundFrom.getMaxY() >= boundTo.getMaxY()) {
 
-            // The line be drawn from the middle of the vertex to be drawn to
-            if (boundFrom.getMinY() <= boundTo.getMinY()
-                    && boundFrom.getMaxY() >= boundTo.getMaxY()) {
-
-                line.setEndX(boundTo.getMinX());
-                line.setStartY(boundTo.getMinY() + boundTo.getHeight() / 2);
-                line.setEndY(boundTo.getMinY() + boundTo.getHeight() / 2);
-
-            } else {
-
-                Pair p0 = new Pair(boundFrom.getMaxX(), boundFrom.getMinY());
-                Pair t0 = new Pair(boundTo.getMinX(), boundTo.getMaxY());
-                Pair p1 = new Pair(boundFrom.getMaxX(), boundFrom.getMaxY());
-                Pair t1 = new Pair(boundTo.getMinX(), boundTo.getMinY());
-
-                double intersect = calculateYIntersection(p0, t0, p1, t1);
-
-                line.setEndX(boundTo.getMinX() + (boundTo.getWidth() / 2));
-                line.setStartY(intersect);
-                line.setEndY(intersect);
-            }
-
-            head.setCenterX(line.getEndX() - head.getRadius());
-            head.setCenterY(line.getEndY());
+            drawLine(startX, boundTo.getMinY() + boundTo.getHeight() / 2,
+                    boundTo.getMinX(), boundTo.getMinY() + boundTo.getHeight()
+                    / 2);
 
         } else {
-            // Draw the line from the middle of both Vertices
+            Pair p0 = new Pair(boundFrom.getMaxX(), boundFrom.getMinY());
+            Pair t0 = new Pair(boundTo.getMinX(), boundTo.getMaxY());
+            Pair p1 = new Pair(boundFrom.getMaxX(), boundFrom.getMaxY());
+            Pair t1 = new Pair(boundTo.getMinX(), boundTo.getMinY());
 
-            double fromX = from.getLayoutX() + (boundFrom.getWidth() / 2);
-            double fromY = from.getLayoutY() + (boundFrom.getHeight() / 2);
+            double intersect = calculateYIntersection(p0, t0, p1, t1);
 
-            // Draw the line from the middle of the destination
-            double toX = to.getLayoutX();
-            double toY = to.getLayoutY() + (boundTo.getHeight() / 2);
+            drawLine(startX, intersect, boundTo.getMinX()
+                    + (boundTo.getWidth() / 2), intersect);
+        }
+        head.setCenterX(line.getEndX() - head.getRadius());
+        head.setCenterY(line.getEndY());
 
-            line.setStartX(fromX);
-            line.setStartY(fromY);
-            line.setEndX(toX);
-            line.setEndY(toY);
+        this.getChildren().addAll(line, head);
+    }
 
-            head.setCenterX(line.getEndX() - head.getRadius());
+    /**
+     * Draw a Crossed line between two Vertices.
+     *
+     * @param from
+     *            - Start Vertex
+     * @param to
+     *            - Destination Vertex
+     * @param boundFrom
+     *            - The Bounds of the start Vertex
+     * @param boundTo
+     *            - The Bounds of the end Vertex
+     */
+    private void drawCrossLine(final Vertex from, final Vertex to,
+            final Bounds boundFrom, final Bounds boundTo) {
+        Circle head = new Circle();
+        head.setRadius(HEAD_RADIUS);
 
-            // Check if the head has to be moved slightly up and down
-            // to display it more clearly
-            if (line.getEndY() - line.getStartY() > 0) {
-                head.setCenterY(line.getEndY() - head.getRadius());
-            }
-            if (line.getEndY() - line.getStartY() < 0) {
-                head.setCenterY(line.getEndY() + head.getRadius());
-            }
+        double fromX = from.getLayoutX() + (boundFrom.getWidth() / 2);
+        double fromY = from.getLayoutY() + (boundFrom.getHeight() / 2);
+
+        // Draw the line from the middle of the destination
+        double toX = to.getLayoutX();
+        double toY = to.getLayoutY() + (boundTo.getHeight() / 2);
+
+        drawLine(fromX, fromY, toX, toY);
+
+        head.setCenterX(line.getEndX() - head.getRadius());
+
+        // Check if the head has to be moved slightly up and down
+        // to display it more clearly
+        if (line.getEndY() - line.getStartY() > 0) {
+            head.setCenterY(line.getEndY() - head.getRadius());
+        }
+        if (line.getEndY() - line.getStartY() < 0) {
+            head.setCenterY(line.getEndY() + head.getRadius());
         }
 
         this.getChildren().addAll(line, head);
+    }
+
+    /**
+     * Draw a line given the start and end coordinates.
+     *
+     * @param startX
+     *            - starting x coordinate
+     * @param startY
+     *            - starting y coordinate
+     * @param endX
+     *            - ending x coordinate
+     * @param endY
+     *            - ending y coordinate
+     */
+    private void drawLine(final double startX, final double startY,
+            final double endX, final double endY) {
+        line.setStartX(startX);
+        line.setStartY(startY);
+        line.setEndX(endX);
+        line.setEndY(endY);
     }
 
     /**
@@ -150,39 +199,23 @@ public class EdgeLine extends Group {
         double s32y = t1.getY() - p1.getY();
 
         double denom = s10x * s32y - s32x * s10y;
-
-        // Collinear
-        if (denom == 0) {
-            return -1;
-        }
-
         Boolean denomPositive = denom > 0;
 
         double s02x = p0.getX() - p1.getX();
         double s02y = p0.getY() - p1.getY();
         double snumer = s10x * s02y - s10y * s02x;
-
-        // No collision
-        if ((snumer < 0) == denomPositive) {
-            return -1;
-        }
-
         double tnumer = s32x * s02y - s32y * s02x;
 
         // No collision
-        if ((tnumer < 0) == denomPositive) {
-            return -1;
-        }
-
-        // No collision
-        if (((snumer > denom) == denomPositive)
+        if ((denom == 0) || (snumer < 0) == denomPositive
+                || (tnumer < 0) == denomPositive
+                || ((snumer > denom) == denomPositive)
                 || ((tnumer > denom) == denomPositive)) {
             return -1;
         }
 
         // Collision detected
         return p0.getY() + ((tnumer / denom) * s10y);
-
     }
 
     /**
