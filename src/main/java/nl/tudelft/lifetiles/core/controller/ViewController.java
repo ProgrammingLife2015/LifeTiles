b433ba45f9954805edbbbcaffb1a8a3a2de2352e
joren <jhammudoglu@gmail.com>
2015-05-10 17:18:59 +0200

    Resturctured packages into the modular MVC structure

---
+++ src/main/java/nl/tudelft/lifetiles/core/controller/ViewController.java
@@ -0,0 +1,69 @@
+package nl.tudelft.lifetiles.core.controller;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Set;
+
+import nl.tudelft.lifetiles.graph.models.sequence.Sequence;
+
+/**
+ * Controls what the view modules display.
+ *
+ * @author Rutger van den Berg
+ */
+public class ViewController extends Observable {
+    /**
+     * Set containing all sequences currently loaded.
+     */
+    private Set<Sequence> allSequences;
+    /**
+     * Map of all sequences currently loaded.
+     */
+    private Map<String, Sequence> sequenceMap;
+    /**
+     * Set containing the currently visible sequences.
+     */
+    private Set<Sequence> visibleSequences;
+
+    /**
+     * Creates a new viewcontroller.
+     *
+     * @param sequences
+     *            The current sequences.
+     */
+    public ViewController(final Map<String, Sequence> sequences) {
+        sequenceMap = sequences;
+        allSequences = new HashSet<>();
+
+        for (Map.Entry<String, Sequence> map : sequenceMap.entrySet()) {
+            Sequence seq = map.getValue();
+            allSequences.add(seq);
+        }
+        visibleSequences = new HashSet<>(allSequences);
+    }
+
+    /**
+     * @return A set containing all visible sequences.
+     */
+    public final Set<Sequence> getVisible() {
+        return visibleSequences;
+    }
+
+    /**
+     * Sets the visible sequences in all views to the provided sequences.
+     *
+     * @param sequences
+     *            The sequences to set to visible.
+     */
+    public final void setVisible(final Set<Sequence> sequences) {
+        visibleSequences = sequences;
+        if (visibleSequences.retainAll(allSequences)) {
+            throw new IllegalArgumentException(
+                    "Attempted to set a non-existant sequence to visible");
+        }
+        setChanged();
+        notifyObservers();
+    }
+
+}
---
+++ src/main/java/nl/tudelft/lifetiles/tree/model/PhylogeneticTreeItem.java
@@ -0,0 +1,235 @@
+package nl.tudelft.lifetiles.tree.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A tree to store the relation between samples.
+ *
+ * @author Albert Smit
+ *
+ */
+public class PhylogeneticTreeItem {
+    /**
+     * Used to give each node a unique id.
+     */
+    private static AtomicInteger nextID = new AtomicInteger();
+
+    /**
+     * The list of children of this node.
+     */
+    private List<PhylogeneticTreeItem> children;
+    /**
+     * The distance between samples. This is an optinal field.
+     */
+    private double distance;
+
+    /**
+     * A unique id for each node.
+     */
+    private int id;
+    /**
+     * The name of the sample. This is an optional field.
+     */
+    private String name;
+    /**
+     * The parent node, null when this node is the root node.
+     */
+    private PhylogeneticTreeItem parent;
+
+    /**
+     * Creates a new PhylogeneticTreeItem. Will initialize the ArrayList storing
+     * the children and assign a new and unique id to the node.
+     */
+    public PhylogeneticTreeItem() {
+        children = new ArrayList<PhylogeneticTreeItem>();
+        this.id = nextID.incrementAndGet();
+    }
+
+    /**
+     * Adds a child to the PhylogeneticTreeItem. This method will add the
+     * PhylogeneticTreeItem child to the ArrayList storing the children of this
+     * node.
+     *
+     * @param child
+     *            the PhylogeneticTreeItem that needs to be added to the tree
+     */
+    public final void addChild(final PhylogeneticTreeItem child) {
+        children.add(child);
+    }
+
+    /**
+     * Compares this with another Object. returns true when both are the same.
+     * two PhylogeneticTreeItems are considered the same when both have the
+     * same:
+     *
+     * <ol>
+     * <li>name or both have no name</li>
+     * <li>distance</li>
+     * <li>children, order does not matter</li>
+     * </ol>
+     *
+     * @param other
+     *            the object to compare with
+     *
+     * @return true if both are the same, otherwise false
+     */
+
+    @Override
+    public final boolean equals(final Object other) {
+        if (other == null) {
+            return false;
+        } else if (other == this) {
+            return true;
+        } else if (other instanceof PhylogeneticTreeItem) {
+            PhylogeneticTreeItem that = (PhylogeneticTreeItem) other;
+            boolean result;
+            // compare name
+            if (name == null && that.getName() == null) {
+                // both are empty and thus the same
+                result = true;
+            } else if (name == null) {
+                // the names are not both empty so not the same
+                result = false;
+            } else {
+                // name is not null check if it is the same
+                result = name.equals(that.getName());
+            }
+
+            // compare distance
+            result = result && (distance == that.getDistance());
+
+            // compare children
+            for (PhylogeneticTreeItem child : children) {
+                result = result && that.getChildren().contains(child);
+            }
+            return result;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the ArrayList of children.
+     *
+     * @return the ArrayList containing all children of this node
+     */
+    public final List<PhylogeneticTreeItem> getChildren() {
+        return children;
+    }
+
+    /**
+     * Returns the distance stored in this node. distance is an optional
+     * property, so the distance can often be 0.0.
+     *
+     * @return the distance of this node
+     */
+    public final double getDistance() {
+        return distance;
+    }
+
+    /**
+     * Returns the Id of this node. because name and distance are optional this
+     * provides an easy way of identifying nodes
+     *
+     * @return the unique id of this PhylogeneticTreeItem
+     */
+    public final int getId() {
+        return id;
+    }
+
+    /**
+     * Returns the name stored in this node. name is an optional property, so
+     * this method can return null.
+     *
+     * @return the name of this node
+     */
+    public final String getName() {
+        return name;
+    }
+
+    /**
+     * Returns this nodes parent node.
+     *
+     * @return the PhylogeneticTreeItem that is this nodes parent
+     */
+    public final PhylogeneticTreeItem getParent() {
+        return parent;
+    }
+
+    /**
+     * Calculates a hash for the tree.
+     */
+    @Override
+    public final int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + children.hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(distance);
+        result = prime * result + (int) (temp ^ (temp >>> prime + 1));
+        if (name != null) {
+            result = prime * result + name.hashCode();
+        } else {
+            result = prime * result;
+        }
+        return result;
+    }
+
+    /**
+     * Sets this nodes distance to the passed double.
+     *
+     * @param d
+     *            the distance between the nodes
+     */
+    public final void setDistance(final double d) {
+        this.distance = d;
+    }
+
+    /**
+     * Set this nodes name to the passed String.
+     *
+     * @param n
+     *            the name of this node
+     */
+    public final void setName(final String n) {
+        this.name = n;
+    }
+
+    /**
+     * Sets the parent to be the node passed to the method This method will set
+     * the parent and also add itself to the list of children in the parent
+     * node.
+     *
+     * @param parentNode
+     *            the node that will be this nodes parent
+     */
+    public final void setParent(final PhylogeneticTreeItem parentNode) {
+        this.parent = parentNode;
+        this.parent.addChild(this);
+    }
+
+    /**
+     * Returns a String representation of the PhylogeneticTreeItem. The String
+     * will have the following format:
+     * <code>&lt;Node: id, name: name, Distance:distance, parent: parentID&gt;
+     * </code> or when the PhylogeneticTreeItem has no parent:
+     * <code>&lt;Node: id, Name: name, Distance: distance, ROOT&gt;</code>
+     *
+     * @return the String version of the object.
+     */
+    @Override
+    public final String toString() {
+        String result = "<Node: " + id + ", Name: " + name + ", Distance: "
+                + distance;
+        if (parent != null) {
+            result += ", parent: " + parent.getId();
+        } else {
+            result += ", ROOT ";
+        }
+        return result + ">";
+    }
+
+}
