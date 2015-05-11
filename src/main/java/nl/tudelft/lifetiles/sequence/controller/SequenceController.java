9b7c282e7067cd073aa90b276ea536502d3df0f2
jorenham <jhammudoglu@mgmail.com>
2015-05-11 15:30:12 +0200

    Created basic sequence view content layout.

--- src/main/java/nl/tudelft/lifetiles/sequence/controller/SequenceController.java
+++ src/main/java/nl/tudelft/lifetiles/sequence/controller/SequenceController.java
@@ -1,10 +1,15 @@
 package nl.tudelft.lifetiles.sequence.controller;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -20,10 +25,33 @@ public class SequenceController implements Initializable {
      */
     @FXML
     private AnchorPane wrapper;
+    /**
+     * Contains the sequences.
+     */
+    @FXML
+    private ListView<Label> sequenceList;
 
     @Override
-    public void initialize(final URL location, final ResourceBundle resources) {
-        // Initialize.
+    public final void initialize(final URL location,
+            final ResourceBundle resources) {
+        ObservableList<String> sequences = FXCollections.observableList(Arrays
+                .asList("TKK-REF", "TKK-01-0066", "TKK-01-0058", "TKK-01-0026",
+                        "TKK-01-0029", "TKK-01-0015"));
+        sequenceList.setItems(label(sequences));
+    }
+
+    /**
+     * Helper method, embeds the strings in a list in labels.
+     *
+     * @param strings the list of strings
+     * @return a list of labels
+     */
+    private ObservableList<Label> label(final ObservableList<String> strings) {
+        ObservableList<Label> res = FXCollections.observableArrayList();
+        for (String s : strings) {
+            res.add(new Label(s));
+        }
+        return res;
     }
 
 }
