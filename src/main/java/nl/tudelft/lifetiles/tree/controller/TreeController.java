67bd3a2c02f0bbdbd2348a0d04a88cb1a855529f
Albert Smit <albertsmit93@gmail.com>
2015-05-12 10:01:35 +0200

    started with custom sunburst control

--- src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
+++ src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
@@ -3,9 +3,10 @@ package nl.tudelft.lifetiles.tree.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import nl.tudelft.lifetiles.tree.view.SunburstView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * The controller of the tree view.
@@ -19,11 +20,13 @@ public class TreeController implements Initializable {
      * The wrapper element.
      */
     @FXML
-    private AnchorPane wrapper;
+    private BorderPane wrapper;
+    
+    private SunburstView view;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        // Initialize.
+        view = new SunburstView();
     }
 
 }
---
+++ src/main/java/nl/tudelft/lifetiles/tree/view/SunburstRing.java
@@ -0,0 +1,9 @@
+package nl.tudelft.lifetiles.tree.view;
+
+public class SunburstRing extends SunburstUnit {
+
+    public SunburstRing() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
