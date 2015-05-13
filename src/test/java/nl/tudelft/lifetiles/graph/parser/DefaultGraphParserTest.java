ce5e2fa8e95ef177b629957512cf9057105b302d
Jos Winter <info@joswinter.nl>
2015-05-13 10:53:06 +0200

    Added DefaultGraphParserTest
    tests the number of vertices and edges in the parsed graph.

---
+++ src/test/java/nl/tudelft/lifetiles/graph/parser/DefaultGraphParserTest.java
@@ -0,0 +1,26 @@
+package nl.tudelft.lifetiles.graph.parser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import nl.tudelft.lifetiles.graph.models.DefaultGraphParser;
+import nl.tudelft.lifetiles.graph.models.FactoryProducer;
+import nl.tudelft.lifetiles.graph.models.Graph;
+import nl.tudelft.lifetiles.graph.models.GraphFactory;
+import nl.tudelft.lifetiles.graph.models.sequence.SequenceSegment;
+
+public class DefaultGraphParserTest {
+
+    @Test
+    public void parseGraphTest() {
+        FactoryProducer<SequenceSegment> fp = new FactoryProducer<SequenceSegment>();
+        GraphFactory<SequenceSegment> gf = fp.getFactory("JGraphT");
+        DefaultGraphParser parser = new DefaultGraphParser();
+        Graph<SequenceSegment> gr = parser.parseFile(
+                "data/simple_graph/simple_graph", gf);
+        assertEquals(2849, gr.getAllVertices().size());
+        assertEquals(3842, gr.getAllEdges().size());
+    }
+}
+
