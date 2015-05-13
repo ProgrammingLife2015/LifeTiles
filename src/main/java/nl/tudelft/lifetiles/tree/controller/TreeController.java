260b90e70a968e71837acb930a82f978e5f4341e
Albert Smit <albertsmit93@gmail.com>
2015-05-13 10:20:42 +0200

    sunburstView now generates sunburstunits for all nodes in the tree they are not placed in the right spot yet, and have a wrong shape

--- src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
+++ src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
@@ -3,6 +3,8 @@ package nl.tudelft.lifetiles.tree.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import nl.tudelft.lifetiles.tree.model.PhylogeneticTreeFactory;
+import nl.tudelft.lifetiles.tree.model.PhylogeneticTreeItem;
 import nl.tudelft.lifetiles.tree.view.SunburstView;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,10 +25,17 @@ public class TreeController implements Initializable {
     private BorderPane wrapper;
     
     private SunburstView view;
+    
+    private PhylogeneticTreeFactory np;
+    private PhylogeneticTreeItem root;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        view = new SunburstView();
+        np = new PhylogeneticTreeFactory("(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;");
+        root = np.getRoot();
+        
+        view = new SunburstView(root);
+        wrapper.setCenter(view);
     }
 
 }
--- src/main/java/nl/tudelft/lifetiles/tree/view/SunburstView.java
+++ src/main/java/nl/tudelft/lifetiles/tree/view/SunburstView.java
@@ -2,28 +2,78 @@ package nl.tudelft.lifetiles.tree.view;
 
 import nl.tudelft.lifetiles.tree.model.PhylogeneticTreeItem;
 import javafx.scene.control.Control;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class SunburstView extends Control{
+public class SunburstView extends Control {
 
     PhylogeneticTreeItem rootItem;
     PhylogeneticTreeItem currentItem;
-    
+    HBox container;
+
     private double factor;
     private double centerX;
     private double centerY;
-    
+
     public SunburstView() {
         super();
-        centerY = getHeight() / 2;
-        centerX = getWidth() / 2 ;
-        PhylogeneticTreeItem root = new PhylogeneticTreeItem();
-        root.setName("Root");
-        SunburstUnit center = new SunburstCenter(root);
-        //center.relocate(centerX, centerY);
-        getChildren().add(center);
+        container = new HBox();
+        getChildren().add(container);
+
+    }
+
+    public SunburstView(PhylogeneticTreeItem root) {
+        super();
+        container = new HBox();
+        getChildren().add(container);
+
+        rootItem = root;
+        selectNode(rootItem);
+        
+    }
+
+    public void selectNode(PhylogeneticTreeItem selected) {
+        currentItem = selected;
+        update();
+    }
+
+    private void update() {
+        container.getChildren().clear();
+        SunburstCenter Center = new SunburstCenter(currentItem);
+        container.getChildren().add(Center);
+
+        int totalDescendants = currentItem.numberDescendants();
+        double degreeStart = 0;
+        for (PhylogeneticTreeItem child : currentItem.getChildren()) {
+            double sectorSize = child.numberDescendants() / totalDescendants;
+            double degreeEnd = degreeStart + (360 * sectorSize);
+
+            drawRingRecursive(child, 0, degreeStart, degreeEnd);
+            degreeStart = degreeEnd;
+        }
+    }
+
+    private void drawRingRecursive(PhylogeneticTreeItem node, int layer,
+            double degreeStart, double degreeEnd) {
+        // generate ring
+        SunburstRing ringUnit = new SunburstRing(node, layer, degreeStart,
+                degreeEnd);
+        container.getChildren().add(ringUnit);
+
+        int totalDescendants = currentItem.numberDescendants();
+        double start = degreeStart;
+        double sectorAngle = SunburstUnit
+                .calculateAngle(degreeStart, degreeEnd);
+        for (PhylogeneticTreeItem child : node.getChildren()) {
+            double sectorSize = child.numberDescendants() / totalDescendants;
+            double end = start + (sectorAngle * sectorSize);
+
+            drawRingRecursive(child, layer + 1, start, end);
+            start = end;
+        }
+
     }
 
 }
