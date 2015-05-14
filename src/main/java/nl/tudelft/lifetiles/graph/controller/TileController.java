beeaeb916d4efb45490b00d73329556f27c3fdc2
joswinter <info@joswinter.nl>
2015-05-14 14:02:39 +0200

    Removed unused error prone function TileController

--- src/main/java/nl/tudelft/lifetiles/graph/controller/TileController.java
+++ src/main/java/nl/tudelft/lifetiles/graph/controller/TileController.java
@@ -38,44 +38,12 @@ public class TileController {
     }
 
     /**
-     * Change the Colour of a Vertex.
-     *
-     * @param color
-     *            new color
-     * @param v
-     *            to be changed vertex
-     */
-    public final void changeColour(final Color color, final VertexView v) {
-        List<Object> args = new ArrayList<Object>();
-        args.add(v);
-        args.add(color);
-        setChangedProperty("COLOR_CHANGED", args);
-    }
-
-    /**
      * Creates a drawable object of the graph from the model.
      *
      * @return Group object to be drawn on the screen
      */
     public final Group drawGraph() {
         return viewVar.drawGraph(modelVar.getGraph());
-    }
-
-    /**
-     * set a certain changed property with the arguments.
-     *
-     * @param propertyName
-     *            Name of the property
-     * @param args
-     *            Arguments for the function that needs to be called.
-     */
-    public final void setChangedProperty(final String propertyName,
-            final List<Object> args) {
-
-        if (propertyName.equals("COLOR_CHANGED")) {
-            viewVar.changeVertexColor((VertexView) args.get(0),
-                    (Color) args.get(1));
-        }
     }
 
 }
