9ded12b66579879bfc691875843cd2978a6c6750
Albert Smit <albertsmit93@gmail.com>
2015-05-15 19:18:19 +0200

    added javadoc comments

--- src/main/java/nl/tudelft/lifetiles/tree/view/SunburstCenter.java
+++ src/main/java/nl/tudelft/lifetiles/tree/view/SunburstCenter.java
@@ -7,15 +7,26 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
+/**
+ *
+ * @author Albert Smit
+ *
+ */
 public class SunburstCenter extends SunburstUnit {
-    
-    
 
+
+    /**
+     * Generates an empty SunburstCenter.
+     */
     public SunburstCenter() {
         display = new Circle(CENTER_RADIUS,Color.BLUE);
         getChildren().add(display);
     }
 
+    /**
+     * Generates a SunburstCenter for the node.
+     * @param v the node that this will represent
+     */
     public SunburstCenter(PhylogeneticTreeItem v){
         value = v;
         display = new Circle(CENTER_RADIUS, Color.BLUE);
