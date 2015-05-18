cf6eab88d4d141a58fce13a3ccc44dc39404893d
Albert Smit <albertsmit93@gmail.com>
2015-05-18 10:32:46 +0200

    fixed size of ringUnits on outer rings

--- src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
+++ src/main/java/nl/tudelft/lifetiles/tree/controller/TreeController.java
@@ -13,7 +13,7 @@ import javafx.scene.layout.BorderPane;
 /**
  * The controller of the tree view.
  *
- * @author Joren Hammudoglu
+ * @author Albert Smit
  *
  */
 public class TreeController implements Initializable {
@@ -33,7 +33,7 @@ public class TreeController implements Initializable {
     public void initialize(final URL location, final ResourceBundle resources) {
         np = new PhylogeneticTreeFactory("(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;");
         root = np.getRoot();
-        
+
         view = new SunburstView(root);
         wrapper.setCenter(view);
     }
