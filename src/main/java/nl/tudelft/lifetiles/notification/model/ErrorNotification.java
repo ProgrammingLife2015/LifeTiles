d29406a1e8a3630a6bfef9b015abe23fa3d8326f
jorenham <jhammudoglu@mgmail.com>
2015-05-21 13:16:56 +0200

    Created the notification models.

---
+++ src/main/java/nl/tudelft/lifetiles/notification/model/ErrorNotification.java
@@ -0,0 +1,33 @@
+package nl.tudelft.lifetiles.notification.model;
+
+import javafx.scene.paint.Color;
+
+/**
+ * A notification of an error.
+ *
+ * @author Joren Hammudoglu
+ *
+ */
+public class ErrorNotification extends Notification {
+
+    /**
+     * The prefix of the message.
+     */
+    private static final String PREFIX = "Error: ";
+
+    /**
+     * Create a new error notification.
+     *
+     * @param message
+     *            the error message
+     */
+    public ErrorNotification(final String message) {
+        super(PREFIX + message);
+    }
+
+    @Override
+    public final Color getColor() {
+        return Color.FIREBRICK;
+    }
+
+}
