530c161f3daab147f4383575fc8dc267626187dc
jorenham <jhammudoglu@mgmail.com>
2015-05-25 23:39:20 +0200

    Refactored controller structure.

--- src/main/java/nl/tudelft/lifetiles/core/controller/MainController.java
+++ src/main/java/nl/tudelft/lifetiles/core/controller/MainController.java
@@ -1,15 +1,13 @@
 package nl.tudelft.lifetiles.core.controller;
 
 import java.net.URL;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import nl.tudelft.lifetiles.graph.controller.GraphController;
 
 /**
  * The controller of the main view.
@@ -17,7 +15,7 @@ import javafx.scene.layout.HBox;
  * @author Joren Hammudoglu
  *
  */
-public class MainController implements Initializable, Observer {
+public class MainController extends Controller {
 
     /**
      * The wrapper element.
@@ -37,26 +35,21 @@ public class MainController implements Initializable, Observer {
     @FXML
     private HBox splashPane;
 
-    /**
-     * The view controller.
-     */
-    private ViewController vc;
-
     @Override
     public final void initialize(final URL location,
             final ResourceBundle resources) {
-        vc = ViewController.getInstance();
-        vc.addObserver(this);
+        super.register(Controller.MAIN);
 
         mainSplitPane.setVisible(false);
     }
 
     @Override
-    public final void update(final Observable o, final Object arg) {
-        if (vc.isLoaded()) {
-            splashPane.setVisible(false);
-            mainSplitPane.setVisible(true);
-        }
+    public final void repaint() {
+        GraphController graphController = (GraphController) getController(Controller.GRAPH);
+        boolean loaded = graphController.isLoaded();
+
+        mainSplitPane.setVisible(loaded);
+        splashPane.setVisible(!loaded);
     }
 
 }
--- src/main/java/nl/tudelft/lifetiles/core/controller/WindowControlController.java
+++ src/main/java/nl/tudelft/lifetiles/core/controller/WindowControlController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -14,7 +13,7 @@ import javafx.stage.Stage;
  * @author Joren Hammudoglu
  *
  */
-public class WindowControlController implements Initializable {
+public class WindowControlController extends Controller {
 
     /**
      * The window close button.
@@ -44,8 +43,9 @@ public class WindowControlController implements Initializable {
     }
 
     @Override
-    public void initialize(final URL location, final ResourceBundle resources) {
-        // nothing to do here
+    public final void initialize(final URL location,
+            final ResourceBundle resources) {
+        super.register(Controller.MENU);
     }
 
     /**
