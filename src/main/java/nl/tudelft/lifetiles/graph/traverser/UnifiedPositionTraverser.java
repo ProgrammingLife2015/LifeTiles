0a831a151d79c44e426ef19f08953977dac02225
joswinter <info@joswinter.nl>
2015-05-28 01:20:38 +0200

    Removed hashMap in unifiedPositionTraverser

--- src/main/java/nl/tudelft/lifetiles/graph/traverser/UnifiedPositionTraverser.java
+++ src/main/java/nl/tudelft/lifetiles/graph/traverser/UnifiedPositionTraverser.java
@@ -1,9 +1,7 @@
 package nl.tudelft.lifetiles.graph.traverser;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Calendar;
 
 import nl.tudelft.lifetiles.graph.model.Edge;
 import nl.tudelft.lifetiles.graph.model.Graph;
@@ -22,13 +20,6 @@ public class UnifiedPositionTraverser {
      * Graph which is being traversed.
      */
     private Graph<SequenceSegment> graphVar;
-
-    /**
-     * Map which maps sequence segments to unifiedPositions, used to make
-     * modifications on during traversal. Inserted into graph at end of
-     * traversal to avoid concurrent modification-like exceptions.
-     */
-    private Map<SequenceSegment, Long> unifiedPositions;
 
     /**
      * Traverses the graph, calculates the unified position. Unified positions
@@ -50,17 +41,15 @@ public class UnifiedPositionTraverser {
      * are needed to visualize a comprehensible model of the graph.
      */
     private void unifyGraph() {
-        unifiedPositions = new HashMap<SequenceSegment, Long>();
+        long startTime = Calendar.getInstance().getTimeInMillis();
         for (SequenceSegment vertex : graphVar.getSources()) {
-            unifiedPositions.put(vertex, (long) 1);
+            vertex.setUnifiedStart(1);
+            vertex.setUnifiedEnd(1 + vertex.getContent().getLength());
             unifyVertex(vertex);
         }
-        for (Entry<SequenceSegment, Long> entry : unifiedPositions.entrySet()) {
-            SequenceSegment vertex = entry.getKey();
-            Long position = entry.getValue();
-            vertex.setUnifiedStart(position);
-            vertex.setUnifiedEnd(position + vertex.getContent().getLength());
-        }
+        System.out.println("Graph unified. Took "
+                + (Calendar.getInstance().getTimeInMillis() - startTime)
+                + " ms.");
     }
 
     /**
@@ -70,14 +59,15 @@ public class UnifiedPositionTraverser {
      *            Vertex to be traversed.
      */
     private void unifyVertex(final SequenceSegment vertex) {
-        long vertexPosition = unifiedPositions.get(vertex)
+        long vertexPosition = vertex.getUnifiedStart()
                 + vertex.getContent().getLength();
         for (Edge<SequenceSegment> edge : new ArrayList<Edge<SequenceSegment>>(
                 graphVar.getOutgoing(vertex))) {
             SequenceSegment destination = graphVar.getDestination(edge);
-            if (!unifiedPositions.containsKey(destination)
-                    || unifiedPositions.get(destination) < vertexPosition) {
-                unifiedPositions.put(destination, vertexPosition);
+            if (destination.getUnifiedStart() < vertexPosition) {
+                destination.setUnifiedStart(vertexPosition);
+                destination.setUnifiedEnd(vertexPosition
+                        + destination.getContent().getLength());
                 unifyVertex(destination);
             }
         }
