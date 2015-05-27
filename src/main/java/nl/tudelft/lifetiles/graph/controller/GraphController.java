ef8f11a363569e5e449da17cef7aa7cda3315e0f
joswinter <info@joswinter.nl>
2015-05-28 01:39:05 +0200

    Added EventListener for scrollbar

--- src/main/java/nl/tudelft/lifetiles/graph/controller/GraphController.java
+++ src/main/java/nl/tudelft/lifetiles/graph/controller/GraphController.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.shape.Rectangle;
 import nl.tudelft.lifetiles.core.controller.AbstractController;
 import nl.tudelft.lifetiles.core.controller.MenuController;
 import nl.tudelft.lifetiles.core.util.Message;
@@ -18,6 +20,7 @@ import nl.tudelft.lifetiles.graph.model.GraphContainer;
 import nl.tudelft.lifetiles.graph.model.GraphFactory;
 import nl.tudelft.lifetiles.graph.model.GraphParser;
 import nl.tudelft.lifetiles.graph.view.TileView;
+import nl.tudelft.lifetiles.graph.view.VertexView;
 import nl.tudelft.lifetiles.notification.controller.NotificationController;
 import nl.tudelft.lifetiles.notification.model.NotificationFactory;
 import nl.tudelft.lifetiles.sequence.model.SequenceSegment;
@@ -40,6 +43,11 @@ public class GraphController extends AbstractController {
      * The currently loaded graph.
      */
     private Graph<SequenceSegment> graph;
+
+    /**
+     * Graph node used to draw the update graph based on bucket cache technique.
+     */
+    private Group graphNode;
 
     /**
      * {@inheritDoc}
@@ -104,9 +112,44 @@ public class GraphController extends AbstractController {
             TileView view = new TileView();
             TileController tileController = new TileController(view, model);
 
-            Group root = tileController.drawGraph();
+            Group root = new Group();
+            Rectangle clip = new Rectangle(getMaxUnifiedEnd(graph)
+                    * VertexView.HORIZONTALSCALE, 0);
+            root.getChildren().add(graphNode);
+            root.getChildren().add(clip);
+            repaintPosition(tileController, root, wrapper.hvalueProperty()
+                    .doubleValue());
+            wrapper.hvalueProperty().addListener(
+                    (ChangeListener<? super Number>) (ov, oldVal, newVal) -> {
+                        repaintPosition(tileController, root,
+                                newVal.doubleValue());
+                    });
             wrapper.setContent(root);
         }
     }
 
+    private void repaintPosition(TileController tileController, Group root,
+            double position) {
+        graphNode = new Group();
+        graphNode.getChildren().add(tileController.drawGraph(position));
+        root.getChildren().add(graphNode);
+        wrapper.setContent(root);
+    }
+
+    /**
+     * Get the maximal unified end position based on the sinks of the graph.
+     *
+     * @param graph
+     *            Graph for which the width must be calculated.
+     * @return the maximal unified end position.
+     */
+    private long getMaxUnifiedEnd(final Graph<SequenceSegment> graph) {
+        long max = 0;
+        for (SequenceSegment vertex : graph.getSinks()) {
+            if (max < vertex.getUnifiedEnd()) {
+                max = vertex.getUnifiedEnd();
+            }
+        }
+        return max;
+    }
 }
