19c502830de717718bbf4d64ce09ffa239a89e4a
joswinter <info@joswinter.nl>
2015-05-28 01:08:04 +0200

    Added BucketCache implementation

---
+++ src/main/java/nl/tudelft/lifetiles/bucket/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Holds the models used to bucket cache the sequence segments for improved
+ * graph drawing performance.
+ *
+ * @author Jos Winter
+ *
+ */
+package nl.tudelft.lifetiles.bucket.model;
