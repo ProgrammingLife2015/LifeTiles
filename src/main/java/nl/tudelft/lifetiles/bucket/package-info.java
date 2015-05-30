efcbfc777bed3d36e0e3d7754df63f8971a6a427
joswinter <info@joswinter.nl>
2015-05-30 17:07:16 +0200

    Moved BucketCache to graph.model

--- src/main/java/nl/tudelft/lifetiles/bucket/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Holds the module used to bucket cache the sequence segments for improved
- * graph drawing performance.
- *
- * @author Jos Winter
- *
- */
-package nl.tudelft.lifetiles.bucket;
