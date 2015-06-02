d0cf735503d6d2716863a3f04c19b66d24c69db0
Joren <joren@linux.com>
2015-06-02 11:42:19 +0200

    Improved reference alignment using smart bfs

--- src/main/java/nl/tudelft/lifetiles/graph/traverser/ReferencePositionTraverser.java
+++ src/main/java/nl/tudelft/lifetiles/graph/traverser/ReferencePositionTraverser.java
@@ -1,9 +1,13 @@
 package nl.tudelft.lifetiles.graph.traverser;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
 import nl.tudelft.lifetiles.core.util.Timer;
 import nl.tudelft.lifetiles.graph.model.Edge;
 import nl.tudelft.lifetiles.graph.model.Graph;
-import nl.tudelft.lifetiles.sequence.model.SegmentString;
 import nl.tudelft.lifetiles.sequence.model.Sequence;
 import nl.tudelft.lifetiles.sequence.model.SequenceSegment;
 
@@ -12,6 +16,7 @@ import nl.tudelft.lifetiles.sequence.model.SequenceSegment;
  * the reference sequence.
  *
  * @author Jos
+ * @author Joren Hammudoglu
  *
  */
 public class ReferencePositionTraverser {
@@ -55,9 +60,49 @@ public class ReferencePositionTraverser {
      *            Graph to be traversed.
      */
     private void referenceMapGraphForward(final Graph<SequenceSegment> graph) {
+        // a queue for breadth-first
+        Queue<SequenceSegment> queue = new ArrayDeque<>();
+        Map<SequenceSegment, Integer> waiting = new HashMap<>();
+
+        // initialize the stack
         for (SequenceSegment source : graph.getSources()) {
             source.setReferenceStart(1);
-            referenceMapVertexForward(source, graph);
+            queue.add(source);
+        }
+
+        while (!queue.isEmpty()) {
+            SequenceSegment vertex = queue.poll();
+            long position = vertex.getReferenceStart();
+
+            // Only continue traversal once all incoming paths to this vertex
+            // have been traversed.
+            final int inLinks = graph.getIncoming(vertex).size();
+            if (waiting.containsKey(vertex)) {
+                int newVal = waiting.get(vertex) - 1;
+                if (newVal < 1) {
+                    // not waiting for the vertex anymore, continue traversal
+                    waiting.remove(vertex);
+                } else {
+                    waiting.put(vertex, newVal);
+                    continue;
+                }
+            } else if (inLinks > 1) {
+                waiting.put(vertex, inLinks - 1);
+                continue;
+            }
+
+            if (vertex.getSources().contains(reference)
+                    && !vertex.getContent().isEmpty()) {
+                position += vertex.getContent().getLength();
+            }
+
+            for (Edge<SequenceSegment> edge : graph.getOutgoing(vertex)) {
+                SequenceSegment destination = graph.getDestination(edge);
+                if (destination.getReferenceStart() < position) {
+                    destination.setReferenceStart(position);
+                }
+                queue.add(destination);
+            }
         }
     }
 
@@ -68,6 +113,11 @@ public class ReferencePositionTraverser {
      *            Graph to be traversed.
      */
     private void referenceMapGraphBackward(final Graph<SequenceSegment> graph) {
+        // a queue for breadth-first
+        Queue<SequenceSegment> queue = new ArrayDeque<>();
+        Map<SequenceSegment, Integer> waiting = new HashMap<>();
+
+        // initialize the stack
         for (SequenceSegment sink : graph.getSinks()) {
             if (sink.getSources().contains(reference)) {
                 sink.setReferenceEnd(sink.getReferenceStart()
@@ -75,56 +125,40 @@ public class ReferencePositionTraverser {
             } else {
                 sink.setReferenceEnd(sink.getReferenceStart() - 1);
             }
-            referenceMapVertexBackward(sink, graph);
+            queue.add(sink);
         }
-    }
 
-    /**
-     * Traverses a vertex forward to generate its reference start position.
-     *
-     * @param vertex
-     *            Vertex to be traversed, calculate reference start position.
-     * @param graph
-     *            Graph to be traversed.
-     */
-    private void referenceMapVertexForward(final SequenceSegment vertex,
-            final Graph<SequenceSegment> graph) {
-        long position = vertex.getReferenceStart();
+        while (!queue.isEmpty()) {
+            SequenceSegment vertex = queue.poll();
+            long position = vertex.getReferenceEnd();
 
-        if (vertex.getSources().contains(reference)
-                && vertex.getContent() instanceof SegmentString) {
-            position += vertex.getContent().getLength();
-        }
-        for (Edge<SequenceSegment> edge : graph.getOutgoing(vertex)) {
-            SequenceSegment destination = graph.getDestination(edge);
-            if (destination.getReferenceStart() < position) {
-                destination.setReferenceStart(position);
-                referenceMapVertexForward(destination, graph);
+            // Only continue traversal once all incoming paths to this vertex
+            // have been traversed.
+            final int inLinks = graph.getIncoming(vertex).size();
+            if (waiting.containsKey(vertex)) {
+                int newVal = waiting.get(vertex) - 1;
+                if (newVal < 1) {
+                    // not waiting for the vertex anymore, continue traversal
+                    waiting.remove(vertex);
+                } else {
+                    waiting.put(vertex, newVal);
+                    continue;
+                }
+            } else if (inLinks > 1) {
+                waiting.put(vertex, inLinks - 1);
+                continue;
             }
-        }
-    }
 
-    /**
-     * Traverses a vertex backward to generate its reference end position.
-     *
-     * @param vertex
-     *            Vertex to be traversed, calculate reference end position.
-     * @param graph
-     *            Graph to be traversed.
-     */
-    private void referenceMapVertexBackward(final SequenceSegment vertex,
-            final Graph<SequenceSegment> graph) {
-        long position = vertex.getReferenceEnd();
-
-        if (vertex.getSources().contains(reference)
-                && vertex.getContent() instanceof SegmentString) {
-            position -= vertex.getContent().getLength();
-        }
-        for (Edge<SequenceSegment> edge : graph.getIncoming(vertex)) {
-            SequenceSegment source = graph.getSource(edge);
-            if (source.getReferenceEnd() > position) {
-                source.setReferenceEnd(position);
-                referenceMapVertexBackward(source, graph);
+            if (vertex.getSources().contains(reference)
+                    && !vertex.getContent().isEmpty()) {
+                position -= vertex.getContent().getLength();
+            }
+            for (Edge<SequenceSegment> edge : graph.getIncoming(vertex)) {
+                SequenceSegment source = graph.getSource(edge);
+                if (source.getReferenceEnd() > position) {
+                    source.setReferenceEnd(position);
+                }
+                queue.add(source);
             }
         }
     }
