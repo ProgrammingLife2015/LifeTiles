6f4e499e784d98f5a2815df546e1aba65101d79a
joswinter <info@joswinter.nl>
2015-06-02 20:13:43 +0200

    Fixed ResistanceAnnotation. annotation is mappable

--- src/main/java/nl/tudelft/lifetiles/annotation/model/ResistanceAnnotationMapper.java
+++ src/main/java/nl/tudelft/lifetiles/annotation/model/ResistanceAnnotationMapper.java
@@ -14,11 +14,11 @@ import nl.tudelft.lifetiles.sequence.model.SequenceSegment;
 /**
  * Static class which maps a list of resistance annotations to a graph and
  * returns a map which maps a segment to a list of resistance annotations.
- * 
+ *
  * @author Jos
  *
  */
-public class ResistanceAnnotationMapper {
+public final class ResistanceAnnotationMapper {
 
     /**
      * A static class can't have a public or default constructor.
@@ -29,7 +29,7 @@ public class ResistanceAnnotationMapper {
 
     /**
      * Maps a list of annotations to a graph.
-     * 
+     *
      * @param graph
      *            Graph to annotate the annotations onto.
      * @param annotations
@@ -40,22 +40,20 @@ public class ResistanceAnnotationMapper {
      * @return
      *         Map which maps segments to a list of annotations.
      */
-    public final static Map<SequenceSegment, List<ResistanceAnnotation>> mapAnnotations(
+    public static Map<SequenceSegment, List<ResistanceAnnotation>> mapAnnotations(
             final Graph<SequenceSegment> graph,
             final List<ResistanceAnnotation> annotations,
             final Sequence reference) {
         Set<SequenceSegment> segments = selectReference(graph, reference);
         Map<SequenceSegment, List<ResistanceAnnotation>> annotatedSegments = new HashMap<>();
         for (ResistanceAnnotation annotation : annotations) {
-            SequenceSegment segment = annotation.mapOntoSequence(segments,
+             SequenceSegment segment = annotation.mapOntoSequence(segments,
                     reference);
             if (segment != null) {
-                if (annotatedSegments.containsKey(segment)) {
-                    annotatedSegments.get(segment).add(annotation);
-                } else {
-                    ArrayList<ResistanceAnnotation> annotationList = new ArrayList<>();
-                    annotatedSegments.put(segment, annotationList);
+                if (!annotatedSegments.containsKey(segment)) {
+                    annotatedSegments.put(segment, new ArrayList<>());
                 }
+                annotatedSegments.get(segment).add(annotation);
             }
         }
         return annotatedSegments;
@@ -64,14 +62,14 @@ public class ResistanceAnnotationMapper {
     /**
      * Selects the segments in the graph which are part of the reference
      * sequence and returns them in a list.
-     * 
+     *
      * @param graph
      *            The graph to be searched for reference segments.
      * @param reference
      *            The reference to search for in the segments.
      * @return set of reference segments in the graph.
      */
-    private final static Set<SequenceSegment> selectReference(
+    private static Set<SequenceSegment> selectReference(
             final Graph<SequenceSegment> graph, final Sequence reference) {
         TreeSet<SequenceSegment> segments = new TreeSet<>();
         for (SequenceSegment segment : graph.getAllVertices()) {
