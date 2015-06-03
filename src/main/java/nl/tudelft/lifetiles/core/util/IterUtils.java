d56544497107e06eb0cccd0b2deddd24ec5ce827
Joren <joren@linux.com>
2015-06-03 14:19:19 +0200

    Refactored ReferencePositionTraverser by using a bfs iterator.

---
+++ src/main/java/nl/tudelft/lifetiles/core/util/IterUtils.java
@@ -0,0 +1,32 @@
+package nl.tudelft.lifetiles.core.util;
+
+import java.util.Iterator;
+
+/**
+ * Utilities for iterators and iterables.
+ *
+ * @author Joren Hammudoglu
+ */
+public final class IterUtils {
+
+    /**
+     * Uninstantiable.
+     */
+    private IterUtils() {
+        // noop
+    }
+
+    /**
+     * Create an {@link Iterable} from an {@link Iterator}.
+     *
+     * @param iterator
+     *            the iterator
+     * @param <T>
+     *            the type
+     * @return the iterable
+     */
+    public static <T> Iterable<T> toIterable(final Iterator<T> iterator) {
+        return () -> iterator;
+    }
+
+}
