aa6d4c1495487cf17c8f2ec25f22580a830f09ed
Joren <joren@linux.com>
2015-06-04 15:12:28 +0200

    Implemented reference selection.

--- src/main/java/nl/tudelft/lifetiles/sequence/controller/SequenceController.java
+++ src/main/java/nl/tudelft/lifetiles/sequence/controller/SequenceController.java
@@ -9,16 +9,24 @@ import java.util.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import nl.tudelft.lifetiles.core.controller.AbstractController;
 import nl.tudelft.lifetiles.core.util.ColorUtils;
 import nl.tudelft.lifetiles.core.util.Message;
 import nl.tudelft.lifetiles.graph.controller.GraphController;
 import nl.tudelft.lifetiles.graph.model.Graph;
+import nl.tudelft.lifetiles.graph.traverser.MutationIndicationTraverser;
+import nl.tudelft.lifetiles.graph.traverser.ReferencePositionTraverser;
+import nl.tudelft.lifetiles.notification.controller.NotificationController;
+import nl.tudelft.lifetiles.notification.model.AbstractNotification;
+import nl.tudelft.lifetiles.notification.model.NotificationFactory;
 import nl.tudelft.lifetiles.sequence.SequenceColor;
 import nl.tudelft.lifetiles.sequence.model.Sequence;
+import nl.tudelft.lifetiles.sequence.model.SequenceSegment;
 
 /**
  * The controller of the data view.
@@ -26,7 +34,17 @@ import nl.tudelft.lifetiles.sequence.model.Sequence;
  * @author Joren Hammudoglu
  *
  */
-public class SequenceController extends AbstractController {
+public final class SequenceController extends AbstractController {
+
+    /**
+     * Shout message indicating a sequence has been set as reference.
+     */
+    public static final Message REFERENCE_SET = Message.create("referenceSet");
+
+    /**
+     * The default reference sequence name.
+     */
+    private static final String DEFAULT_REFERENCE = "TKK_REF";
 
     /**
      * Contains the sequences.
@@ -45,28 +63,41 @@ public class SequenceController extends AbstractController {
     private Set<Sequence> visibleSequences;
 
     /**
+     * The reference sequence.
+     */
+    private Sequence reference;
+
+    /**
+     * The loaded graph.
+     */
+    private Graph<SequenceSegment> graph;
+
+    /**
+     * Notification factory.
+     */
+    private final NotificationFactory notFact = new NotificationFactory();
+
+    /**
      * {@inheritDoc}
      */
     @Override
-    public final void initialize(final URL location,
-            final ResourceBundle resources) {
+    public void initialize(final URL location, final ResourceBundle resources) {
         repaint();
 
-        listen(Message.LOADED, (controller, args) -> {
-            if (controller instanceof GraphController) {
+        listen(Message.LOADED, (sender, args) -> {
+            if (sender instanceof GraphController) {
                 assert args[0] instanceof Graph;
                 assert (args[1] instanceof Map<?, ?>);
+                graph = (Graph<SequenceSegment>) args[0];
                 setSequences((Map<String, Sequence>) args[1]);
+                setReference();
                 repaint();
             }
         });
 
-        listen(Message.FILTERED, (controller, args) -> {
+        listen(Message.FILTERED, (sender, args) -> {
             assert args.length == 1;
-            if (!(args[0] instanceof Set<?>)) {
-                throw new IllegalArgumentException(
-                        "Argument not of type Set<Sequence>");
-            }
+            assert (args[0] instanceof Set<?>);
 
             setVisible((Set<Sequence>) args[0], false);
         });
@@ -75,7 +106,7 @@ public class SequenceController extends AbstractController {
     /**
      * @return A set containing all visible sequences.
      */
-    public final Set<Sequence> getVisible() {
+    public Set<Sequence> getVisible() {
         if (visibleSequences == null) {
             throw new IllegalStateException("Sequences not loaded.");
         }
@@ -113,7 +144,7 @@ public class SequenceController extends AbstractController {
      * @param newSequences
      *            the sequences to set
      */
-    public final void setSequences(final Map<String, Sequence> newSequences) {
+    public void setSequences(final Map<String, Sequence> newSequences) {
         sequences = newSequences;
         visibleSequences = new HashSet<>(sequences.values());
     }
@@ -137,6 +168,11 @@ public class SequenceController extends AbstractController {
                 .observableArrayList();
         for (final Sequence sequence : sequences.values()) {
             String identifier = sequence.getIdentifier();
+
+            if (sequence.equals(reference)) {
+                identifier += "*";
+            }
+
             Label label = new Label(identifier);
             Color color = SequenceColor.getColor(sequence);
 
@@ -148,26 +184,99 @@ public class SequenceController extends AbstractController {
                 label.getStyleClass().add(styleClassFilter);
             }
 
-            label.setOnMouseClicked((mouseEvent) -> {
-                Set<Sequence> visible = getVisible();
+            label.setOnMouseClicked(mouseEvent -> {
+                if (mouseEvent.isPrimaryButtonDown()) {
+                    Set<Sequence> visible = getVisible();
 
-                if (label.getStyleClass().contains(styleClassFilter)) {
-                    // hide
-                    visible.remove(sequence);
-                    label.getStyleClass().remove(styleClassFilter);
-                } else {
-                    // show
-                    visible.add(sequence);
-                    label.getStyleClass().add(styleClassFilter);
+                    if (label.getStyleClass().contains(styleClassFilter)) {
+                        // hide
+                        visible.remove(sequence);
+                        label.getStyleClass().remove(styleClassFilter);
+                    } else {
+                        // show
+                        visible.add(sequence);
+                        label.getStyleClass().add(styleClassFilter);
+                    }
+
+                    setVisible(visible, true);
                 }
+            });
 
-                setVisible(visible, true);
+            label.setOnMousePressed(mouseEvent -> {
+                if (mouseEvent.isSecondaryButtonDown()) {
+                    getContextMenu(sequence).show(label,
+                            mouseEvent.getScreenX(), mouseEvent.getScreenY());
+                }
             });
 
             sequenceItems.add(label);
         }
 
         return sequenceItems;
+    }
+
+    /**
+     * Get the sequence context menu.
+     *
+     * @param sequence
+     *            the sequence
+     * @return the context menu
+     */
+    private ContextMenu getContextMenu(final Sequence sequence) {
+        final ContextMenu contextMenu = new ContextMenu();
+        MenuItem setRef = new MenuItem("Set as reference.");
+        contextMenu.getItems().add(setRef);
+        setRef.setOnAction(actionEvent -> {
+            setReference(sequence);
+            repaint();
+        });
+        return contextMenu;
+    }
+
+    /**
+     * Set the default reference as reference.
+     */
+    private void setReference() {
+        Sequence defaultReference = null;
+        for (Sequence other : sequences.values()) {
+            if (other.getIdentifier().equals(DEFAULT_REFERENCE)) {
+                defaultReference = other;
+                break;
+            }
+        }
+
+        if (defaultReference == null) {
+            String message = "The default reference sequence "
+                    + DEFAULT_REFERENCE + " was not found";
+            AbstractNotification notification = notFact.getNotification(
+                    message, NotificationFactory.WARNING);
+            shout(NotificationController.NOTIFY, notification);
+        }
+
+        this.reference = defaultReference;
+    }
+
+    /**
+     * Set a sequence as reference.
+     *
+     * @param sequence
+     *            the new reference sequence
+     */
+    private void setReference(final Sequence sequence) {
+        this.reference = sequence;
+        shout(SequenceController.REFERENCE_SET, sequence);
+
+        new ReferencePositionTraverser(sequence).referenceMapGraph(graph);
+        new MutationIndicationTraverser(sequence).indicateGraphMutations(graph);
+    }
+
+    /**
+     * Get the reference.
+     *
+     * @return the reference
+     */
+    private Sequence getReference() {
+        return reference;
     }
 
 }
