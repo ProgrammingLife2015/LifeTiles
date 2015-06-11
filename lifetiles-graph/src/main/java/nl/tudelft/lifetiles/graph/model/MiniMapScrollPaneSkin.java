07847004de21c27acde58838b8e3cc5c79948ddd
Joren <joren@linux.com>
2015-06-11 14:42:42 +0200

    Basic toolbar customization

---
+++ lifetiles-graph/src/main/java/nl/tudelft/lifetiles/graph/model/MiniMapScrollPaneSkin.java
@@ -0,0 +1,44 @@
+package nl.tudelft.lifetiles.graph.model;
+
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+
+import com.sun.javafx.scene.control.skin.ScrollPaneSkin;
+
+/**
+ * ScrollPane skin capable of customizing the scrollbar.
+ *
+ * @author Joren Hammudoglu
+ */
+@SuppressWarnings("restriction")
+public class MiniMapScrollPaneSkin extends ScrollPaneSkin {
+
+    /**
+     * Create a new MiniMap ScrollPane skin.
+     *
+     * @param scrollpane
+     *            the scrollpane
+     */
+
+    public MiniMapScrollPaneSkin(final ScrollPane scrollpane) {
+        super(scrollpane);
+    }
+
+    /**
+     * @return the horizontal scrollbar
+     */
+    public final ScrollBar getHorizontalScrollBar() {
+        return super.hsb;
+    }
+
+    /**
+     * Set the horizontal scrollbar.
+     *
+     * @param scrollbar
+     *            the scrollbar
+     */
+    public final void setHorizontalScrollbar(final ScrollBar scrollbar) {
+        super.hsb = scrollbar;
+    }
+
+}
