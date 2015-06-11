ff1f5a31721be954c00f689f1256ebb6693df3f3
Albert Smit <albertsmit93@gmail.com>
2015-06-11 14:00:24 +0200

    optimzed setVisible in graphContainer

--- lifetiles-graph/src/main/java/nl/tudelft/lifetiles/graph/model/GraphContainer.java
+++ lifetiles-graph/src/main/java/nl/tudelft/lifetiles/graph/model/GraphContainer.java
@@ -1,5 +1,6 @@
 package nl.tudelft.lifetiles.graph.model;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -117,8 +118,14 @@ public class GraphContainer {
         Timer timer = Timer.getAndStart();
         // Find out which vertices are visible now
         Set<SequenceSegment> vertices = new TreeSet<SequenceSegment>();
-        for (Sequence seq : visibleSequences) {
-            for (SequenceSegment segment : seq.getSegments()) {
+
+        for (SequenceSegment segment: graph.getAllVertices()) {
+            //copy the set of sequences because retainAll modifies the original set
+            Set<Sequence> intersect;
+            intersect = new HashSet<Sequence>(segment.getSources());
+            //check if any of the visible sequences are in this nodes sources
+            intersect.retainAll(visibleSequences);
+            if (!intersect.isEmpty()) {
                 vertices.add(segment);
             }
         }
