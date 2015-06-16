e305b30760c35ac43992294362fd1f9ce60df0bf
joswinter <info@joswinter.nl>
2015-06-16 19:29:10 +0200

    Shout: loaded annotations message for BookmarkView

--- lifetiles-graph/src/test/java/nl/tudelft/lifetiles/annotation/model/GeneAnnotationParserTest.java
+++ lifetiles-graph/src/test/java/nl/tudelft/lifetiles/annotation/model/GeneAnnotationParserTest.java
@@ -3,21 +3,26 @@ package nl.tudelft.lifetiles.annotation.model;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.util.Map;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class GeneAnnotationParserTest {
-    static final String testGenomeFilename = "/data/test_annotations/simple_annotations";
-    private Map<String, GeneAnnotation> genomes;
+    static final String testGenomesFilename = "/data/test_annotations/simple_annotations.gff";
+    static final String testGenomeFilename = "/data/test_annotations/simple_annotation.gff";
+    private Set<GeneAnnotation> genomes;
+    private Set<GeneAnnotation> genome;
 
     @Before
     public void setUp() throws Exception {
+        File genomesFile = new File(this.getClass()
+                .getResource(testGenomesFilename).toURI());
         File genomeFile = new File(this.getClass()
-                .getResource(testGenomeFilename + ".gff").toURI());
+                .getResource(testGenomeFilename).toURI());
 
-        genomes = GeneAnnotationParser.parseGeneAnnotations(genomeFile);
+        genomes = GeneAnnotationParser.parseGeneAnnotations(genomesFile);
+        genome = GeneAnnotationParser.parseGeneAnnotations(genomeFile);
     }
 
     @Test
@@ -26,10 +31,10 @@ public class GeneAnnotationParserTest {
     }
 
     @Test
-    public void parseGenomeSingleTest() throws Exception {
-        GeneAnnotation genome = genomes.get("test1");
-        assertEquals("test1", genome.getName());
-        assertEquals(1, genome.getGenomePosition());
-        assertEquals(1000, genome.getGenomeEndPosition());
+    public void parseGenomeSingleTest() throws Exception {        
+        GeneAnnotation gene = genome.iterator().next();
+        assertEquals("test1", gene.getName());
+        assertEquals(1, gene.getGenomePosition());
+        assertEquals(1000, gene.getGenomeEndPosition());
     }
 }
