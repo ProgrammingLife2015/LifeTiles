81d1103ae3ec56239aa6f87d30dce79e35ca87f2
Jos Winter <info@joswinter.nl>
2015-06-19 15:42:49 +0200

    Set logging level to off in LoggingTest

--- lifetiles-core/src/test/java/nl/tudelft/lifetiles/core/util/LoggingTest.java
+++ lifetiles-core/src/test/java/nl/tudelft/lifetiles/core/util/LoggingTest.java
@@ -1,8 +1,16 @@
 package nl.tudelft.lifetiles.core.util;
 
+import java.util.logging.Level;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggingTest {
+
+    @BeforeClass
+    public static void before() {
+        Logging.setLevel(Level.OFF);
+    }
 
     @Test
     public void warningTest() {
